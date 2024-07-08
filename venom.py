86aaea16f6ca8320481baa26a903c20c3a3ae55e
HYPER-prog-deal <hypergamer6336@gmail.com>
2024-07-08 14:18:38 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7010034196:AAGS8ov4M6Cv7QjTFCUOt44jL_uuKsNpcEo'
+ADMIN_ID = '7413639068'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
