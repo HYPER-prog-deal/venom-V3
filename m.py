68e1b41a4181db12a42595f069c4d9b7f0345ece
HYPER-prog-deal <hypergamer6336@gmail.com>
2024-07-08 14:17:09 +0530

    Update m.py

--- m.py
+++ m.py
@@ -10,9 +10,9 @@ from keep_alive import keep_alive
 keep_alive()
 
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEM')
+bot = telebot.TeleBot('7010034196:AAGS8ov4M6Cv7QjTFCUOt44jL_uuKsNpcEo')
 # Admin user IDs
-admin_id = {"5588464519"}
+admin_id = {"7413639068"}
 
 # Files for data storage
 USER_FILE = "users.json"
